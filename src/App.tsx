98bdf7b2f4d327a429588dca8cb716c4788fec86
Matheus Lima <eclicknoplay@gmail.com>
2022-11-06 22:31:37 -0400

    Hello World

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css';
+import { Box } from '@chakra-ui/react';
+
+function App() {
+  return (
+    <Box className='App'>
+      <h1>Hello World</h1>
+    </Box>
+  );
+}
+
+export default App;
